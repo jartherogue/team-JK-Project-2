186ba2e4f0774a438d72dd665842b40f878c5e15
jartherogue <jasreev@gmail.com>
2018-05-09 19:30:18 -0600

    Added Kristen's work from yesterday

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,58 +1,61 @@
 var db = require("../models");
 
+// Routes
+// =============================================================
 module.exports = function(app) {
 
-     // GET route for getting all of the todos
-  app.get("/api/todos", function(req, res) {
-    // findAll returns all entries for a table when used with no options
-    db.Todo.findAll({}).then(function(dbTodo) {
-      // We have access to the todos as an argument inside of the callback function
-      res.json(dbTodo);
-    });
-  });
+ // GET route for getting all of the todos
+ app.get("/api/activities", function(req, res) {
 
-  // POST route for saving a new todo
-  app.post("/api/users", function(req, res) {
-    // create takes an argument of an object describing the item we want to
-    // insert into our table. In this case we just we pass in an object with a text
-    // and complete property
-    db.Todo.create({
-      text: req.body.text,
-      complete: req.body.complete
-    }).then(function(dbTodo) {
-      // We have access to the new todo as an argument inside of the callback function
-      res.json(dbTodo);
-    });
-  });
+   db.Activitie.findAll({}).then(function(results) {
+     res.json(results);
+   });
+   // Write code here to retrieve all of the todos from the database and res.json them
+   // back to the user
+ });
 
-  // DELETE route for deleting todos. We can get the id of the todo to be deleted from
-  // req.params.id
-  app.delete("/api/todos/:id", function(req, res) {
-    // We just have to specify which todo we want to destroy with "where"
-    db.Todo.destroy({
-      where: {
-        id: req.params.id
-      }
-    }).then(function(dbTodo) {
-      res.json(dbTodo);
-    });
+ // POST route for saving a new todo. We can create todo with the data in req.body
+ app.post("/api/activities", function(req, res) {
 
-  });
+   var activities = req.body.text;
 
-  // PUT route for updating todos. We can get the updated todo data from req.body
-  app.put("/api/todos", function(req, res) {
-    // Update takes in an object describing the properties we want to update, and
-    // we use where to describe which objects we want to update
-    db.Todo.update({
-      text: req.body.text,
-      complete: req.body.complete
-    }, {
-      where: {
-        id: req.body.id
-      }
-    }).then(function(dbTodo) {
-      res.json(dbTodo);
-    });
-  });
+   if (req.params.activities) {
+   db.Activitie.create({
+     text: req.body.text,
+     complete: req.body.complete
+   }).then(function(result) {
+     return res.json(result);
+   });
+   // Write code here to create a new todo and save it to the database
+   // and then res.json back the new todo to the user
+ };
 
+
+
+ // DELETE route for deleting todos. We can get the id of the todo to be deleted from
+ // req.params.id
+ app.delete("/api/activities/:id", function(req, res) {
+   db.Activitie.destroy({
+     text: req.body.text,
+     complete: req.body.complete
+   }).then(function(dbActivitie) {
+     // We have access to the new todo as an argument inside of the callback function
+     res.json(dbActivitie);
+   // Use the sequelize destroy method to delete a record from our table with the
+   // id in req.params.id. res.json the result back to the user
+ });
+ });
+
+ // PUT route for updating todos. We can get the updated todo data from req.body
+ app.put("/api/activities", function(req, res) {
+   db.Activitie.update({
+     where: {
+       text: req.params.text,
+       complete: req.params.complete
+     }
+   }).then(function(results) {
+     res.json(dbActivitie);
+   });
+   });
+ });
 };
